7bb0122defa76d8c1a8141dff2ff20f8da0bd015
Alok sharma <alok.sharma.cse1@gmail.com>
2026-03-12 23:43:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,5 +26,5 @@ app.use("/api/payment" , paymentRouter)
 const PORT = process.env.PORT || 6000
 app.listen(PORT , ()=>{
     console.log(`Server running on port ${PORT}`)
-    connectDb()
+    connectDB()
 })
